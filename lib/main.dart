788306e0e003950d777a34b59cde7cbdbc83f17d
najeed <najid.habib@gmail.com>
2023-06-24 13:22:12 +0500

    icons and transparency

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz_start.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.deepPurple,
-      body: Center(child: QuizStart()),
-    ),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.deepPurple, Colors.blueGrey],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: const QuizStart(),
+        ),
+      ),
+    );
+  }
 }
--- lib/quiz_start.dart
+++ lib/quiz_start.dart
@@ -5,30 +5,35 @@ class QuizStart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 350,
-        ),
-        const SizedBox(
-          height: 40,
-        ),
-        const Text(
-          'Learn Flutter the Fun Way!',
-          style: TextStyle(fontSize: 20, color: Colors.white),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const ElevatedButton(
-            onPressed: null,
-            child: Text(
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 350,
+            color: const Color.fromARGB(119, 251, 251, 251),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            'Learn Flutter the Fun Way!',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton.icon(
+            onPressed: () {},
+            label: const Text(
               'Start Quiz',
               style: TextStyle(color: Colors.white),
-            ))
-      ],
+            ),
+            icon: const Icon(Icons.arrow_forward_sharp),
+          ),
+        ],
+      ),
     );
   }
 }
